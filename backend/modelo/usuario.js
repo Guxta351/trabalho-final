e233a9ed02359b69092d38c287f9aad9e85dfc3e
Eloizasocoloski <eloizasocoloskipereira15@gmail.com>
2024-08-27 21:22:28 -0300

    back da elo

---
+++ backend/modelo/usuario.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Usuario extends Model {}
+
+Usuario.init({
+    id_Usuario: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Nome_Usuario: {
+        type: DataTypes.STRING
+    },
+    Email: {
+        type: DataTypes.TEXT
+    },
+    Senha_unitario: {
+        type: DataTypes.TEXT
+
+    },
+    Endereco: {
+        type : DataTypes.TEXT
+    }
+    
+}, {
+    sequelize,
+    modelName: 'Usuario',
+    timestamps: false
+});
+
+module.exports = Usuario;
