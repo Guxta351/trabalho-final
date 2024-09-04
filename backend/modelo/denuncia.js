fab2ef8cc4ec4316c75899b7185e610987e7fb29
José Marques <jose8332905@gmail.com>
2024-09-03 21:36:11 -0300

    inicio do back denuncia

---
+++ backend/modelo/denuncia.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Denuncia extends Model {}
+
+Usuario.init({
+    id_denuncia: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    data_criacao: {
+        type: DataTypes.DATE
+    },
+    descricao: {
+        type: DataTypes.TEXT
+    },
+    localizacao: {
+        type: DataTypes.TEXT
+    },
+    
+}, {
+    sequelize,
+    modelName: 'Usuario',
+    timestamps: false
+});
+
+module.exports = Usuario;
